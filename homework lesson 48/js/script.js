1849b5ec2869d6c698d55a07ab83ab78c7d4a1d8
Spaun1 <andreispaun7@gmail.com>
2022-06-28 23:58:13 +0300

    homework for lesson 48

--- homework lesson 48/js/script.js
+++ homework lesson 48/js/script.js
@@ -1,55 +1,106 @@
-/* Задания на урок: 
+/* Задания на урок:
 
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться. 
-Новый фильм должен добавляться в movieDB.movies. 
-Для получения доступа к значению input - обращаемся к нему как input.value; 
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий. 
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" -
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки 
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно) 
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение:  
-"Добавляем любимый фильм" 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение:
+"Добавляем любимый фильм"
 
-5) Фильмы должны быть отсортированы по алфавиту */ 
+5) Фильмы должны быть отсортированы по алфавиту */
 
-// Возьмите свой код из предыдущей практики 
+// Возьмите свой код из предыдущей практики
 
 'use strict';
 
-const movieDB = {
-	movies: [
-		'Логан',
-		'Лига справедливости',
-		'Ла-ла лэнд',
-		'Одержимость',
-		'Скотт Пилигрим против...',
-	],
-};
+document.addEventListener('DOMContentLoaded', () => {
+	const movieDB = {
+		movies: [
+			'Логан',
+			'Лига справедливости',
+			'Ла-ла лэнд',
+			'Одержимость',
+			'Скотт Пилигрим против...',
+		],
+	};
 
-const advertising = document.querySelectorAll('.promo__adv img'),
-		poster = document.querySelector('.promo__bg'),
-		genre = poster.querySelector('.promo__genre'),
-		movieList = document.querySelector('.promo__interactive-list');
+	const advertising = document.querySelectorAll('.promo__adv img'),
+			poster = document.querySelector('.promo__bg'),
+			genre = poster.querySelector('.promo__genre'),
+			movieList = document.querySelector('.promo__interactive-list'),
+			addForm = document.querySelector('form.add'),
+			addInput = addForm.querySelector('.adding__input'),
+			chekbox = addForm.querySelector('[type="checkbox"]');
 
-advertising.forEach((item) => {
-	item.remove();
-});
+	addForm.addEventListener('submit', (event) => {
+		event.preventDefault();
 
-genre.textContent = 'ДРАМА';
+		let newFilm = addInput.value;
+		const favorite = chekbox.checked;
 
-poster.style.backgroundImage = 'url("/homework lesson 44//img/bg.jpg")';
+		if (newFilm) {
+			if (newFilm.length > 21) {
+				newFilm = `${newFilm.substring(0, 22)}...`;
+			}
 
-movieList.innerHTML = '';
+			if (favorite) {
+				console.log('Добавляем любимый фильм');
+			}
 
-movieDB.movies.sort();
+			movieDB.movies.push(newFilm);
+			sortArr(movieDB.movies);
 
-movieDB.movies.forEach((film, i) => {
-	movieList.innerHTML += `
-	<li class="promo__interactive-item">${i + 1} ${film}
-		<div class="delete"></div>
-	</li>
-	`;
+			createMovieList(movieDB.movies, movieList);
+		}
+
+		event.target.reset();
+	});
+
+	const deleteAdvertising = (arr) => {
+		arr.forEach((item) => {
+			item.remove();
+		});
+	};
+
+	const makeChanges = () => {
+		genre.textContent = 'ДРАМА';
+
+		poster.style.backgroundImage = 'url("/homework lesson 44//img/bg.jpg")';
+	};
+
+	const sortArr = (arr) => {
+		arr.sort();
+	};
+
+	function createMovieList(films, parent) {
+		parent.innerHTML = '';
+		sortArr(films);
+
+		films.forEach((film, i) => {
+			parent.innerHTML += `
+			<li class="promo__interactive-item">${i + 1} ${film}
+				<div class="delete"></div>
+			</li>
+			`;
+		});
+
+		document.querySelectorAll('.delete').forEach((btn, i) => {
+			btn.addEventListener('click', () => {
+				btn.parentElement.remove();
+				movieDB.movies.splice(i, 1);
+
+				createMovieList(films, parent);
+			});
+		});
+	}
+
+	deleteAdvertising(advertising);
+	makeChanges();
+	createMovieList(movieDB.movies, movieList);
 });
